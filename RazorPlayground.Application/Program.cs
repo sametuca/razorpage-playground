b40454b62b58d1ff1b162715e08647be975c9b09
Samet Uca <samet.uca@marti.tech>
2022-11-29 21:34:22 +0300

    added code first configuration and migration

--- RazorPlayground.Application/Program.cs
+++ RazorPlayground.Application/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RazorPlayground.Persistence;
+using RazorPlayground.Persistence.Entities;
 using RazorPlayground.Services.Abstract;
 using RazorPlayground.Services.Concrete;
 
@@ -8,6 +13,19 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<ICustomerService, CustomerService>();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseNpgsql(connectionString, b => b.MigrationsAssembly("RazorPlayground.Application")));
+//dbcontextoptionsbuilder
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<DataContext>();
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Default Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ RazorPlayground.Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RazorPlayground.Persistence.Entities;
+
+namespace RazorPlayground.Persistence
+{
+    public class DataContext : IdentityDbContext<User>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+    }
+}
